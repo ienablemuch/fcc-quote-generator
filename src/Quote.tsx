cad5bc43dabdf5b4f28e59644fa75741826bf552
Michael Buen <ienablemuch@gmail.com>
2021-02-08 14:44:40 +0800

    css for message, author, buttons

---
+++ src/Quote.tsx
@@ -0,0 +1,12 @@
+export function Quote() {
+    return (
+        <div className="quote">
+            <div className="message">Message here</div>
+            <div className="author">Author here</div>
+            <div className="buttons">
+                <div className="button">Twitter</div>
+                <div className="button">Next Quote</div>
+            </div>
+        </div>
+    );
+}
