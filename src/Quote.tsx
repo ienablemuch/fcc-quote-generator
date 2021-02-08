4b55159b13e073214fd1884533f8de3dc03a9da3
Michael Buen <ienablemuch@gmail.com>
2021-02-08 19:32:15 +0800

    quotes randomized

--- src/Quote.tsx
+++ src/Quote.tsx
@@ -1,13 +1,53 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 interface IQuoteProps {
     colorTheme: string;
     onColorRequestChange(): void;
 }
 
+interface IQuote {
+    quote: string;
+    author: string;
+}
+
 export function Quote({ colorTheme, onColorRequestChange }: IQuoteProps) {
     const messageRef = useRef(null);
     const authorRef = useRef(null);
+
+    const [data, setData] = useState<{
+        quotes: IQuote[];
+    }>({ quotes: [] });
+
+    const [randomQuote, setRandomQuote] = useState<IQuote>({
+        quote: "",
+        author: "",
+    });
+
+    useEffect(() => {
+        async function fetchData() {
+            const result = await fetch(
+                "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+            );
+
+            setData(await result.json());
+        }
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        if (data.quotes.length === 0) {
+            return;
+        }
+
+        randomizeQuote();
+    }, [data]);
+
+    function randomizeQuote() {
+        const newRandomQuote = Math.floor(Math.random() * data.quotes.length);
+
+        setRandomQuote(data.quotes[newRandomQuote]);
+    }
 
     function handleNewQuote() {
         const messageElement: any = messageRef.current;
@@ -15,18 +55,19 @@ export function Quote({ colorTheme, onColorRequestChange }: IQuoteProps) {
 
         messageElement.addEventListener(
             "transitionend",
-            handleOpacityTransition
+            handleOpacityTransitionEnd
         );
 
         messageElement.style.opacity = 0;
         authorElement.style.opacity = 0;
 
-        function handleOpacityTransition(ev: any, data: any) {
+        function handleOpacityTransitionEnd(ev: any, data: any) {
             messageElement.removeEventListener(
                 "transitionend",
-                handleOpacityTransition
+                handleOpacityTransitionEnd
             );
 
+            randomizeQuote();
             onColorRequestChange();
             messageElement.style.opacity = 1;
             authorElement.style.opacity = 1;
@@ -40,14 +81,14 @@ export function Quote({ colorTheme, onColorRequestChange }: IQuoteProps) {
                 style={{ color: colorTheme }}
                 ref={messageRef}
             >
-                Message here
+                {randomQuote.quote}
             </div>
             <div
                 className="author"
                 style={{ color: colorTheme }}
                 ref={authorRef}
             >
-                Author here
+                {randomQuote.author}
             </div>
             <div className="buttons">
                 <button
