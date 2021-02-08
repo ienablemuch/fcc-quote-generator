ba55a0f9a6b3078a79a651a4a352f2fba1cf5765
Michael Buen <ienablemuch@gmail.com>
2021-02-08 18:34:51 +0800

    transition color theme

--- src/Quote.tsx
+++ src/Quote.tsx
@@ -1,15 +1,52 @@
+import { useRef, useEffect } from "react";
+
 interface IQuoteProps {
     colorTheme: string;
     onColorRequestChange(): void;
 }
 
 export function Quote({ colorTheme, onColorRequestChange }: IQuoteProps) {
+    const messageRef = useRef(null);
+    const authorRef = useRef(null);
+
+    function handleNewQuote() {
+        const messageElement: any = messageRef.current;
+        const authorElement: any = authorRef.current;
+
+        messageElement.addEventListener(
+            "transitionend",
+            handleOpacityTransition
+        );
+
+        messageElement.style.opacity = 0;
+        authorElement.style.opacity = 0;
+
+        function handleOpacityTransition(ev: any, data: any) {
+            messageElement.removeEventListener(
+                "transitionend",
+                handleOpacityTransition
+            );
+
+            onColorRequestChange();
+            messageElement.style.opacity = 1;
+            authorElement.style.opacity = 1;
+        }
+    }
+
     return (
         <div className="quote">
-            <div className="message" style={{ color: colorTheme }}>
+            <div
+                className="message"
+                style={{ color: colorTheme }}
+                ref={messageRef}
+            >
                 Message here
             </div>
-            <div className="author" style={{ color: colorTheme }}>
+            <div
+                className="author"
+                style={{ color: colorTheme }}
+                ref={authorRef}
+            >
                 Author here
             </div>
             <div className="buttons">
@@ -22,7 +59,7 @@ export function Quote({ colorTheme, onColorRequestChange }: IQuoteProps) {
                 <button
                     className="button"
                     style={{ backgroundColor: colorTheme }}
-                    onClick={onColorRequestChange}
+                    onClick={handleNewQuote}
                 >
                     New Quote
                 </button>
