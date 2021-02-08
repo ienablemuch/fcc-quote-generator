f0e70167fb32bdf6cbdc2b427463ec80b80be9cc
Michael Buen <ienablemuch@gmail.com>
2021-02-08 16:12:35 +0800

    removed hardcoded color theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 
 import "./index.css";
+import { colors } from "./colors";
 
 import { Quote } from "./Quote";
 
 function App() {
+    const [colorIndex, setColorIndex] = useState(0);
+    const [colorTheme, setColorTheme] = useState(colors[colorIndex]);
+
+    useEffect(() => {
+        setColorTheme(colors[colorIndex % colors.length]);
+    }, [colorIndex]);
+
     return (
-        <div className="App">
-            <Quote />
+        <div className="App" style={{ backgroundColor: colorTheme }}>
+            <Quote colorTheme={colorTheme} />
         </div>
     );
 }
--- src/Quote.tsx
+++ src/Quote.tsx
@@ -1,11 +1,23 @@
-export function Quote() {
+interface IQuoteProps {
+    colorTheme: string;
+}
+
+export function Quote({ colorTheme }: IQuoteProps) {
     return (
         <div className="quote">
-            <div className="message">Message here</div>
-            <div className="author">Author here</div>
+            <div className="message" style={{ color: colorTheme }}>
+                Message here
+            </div>
+            <div className="author" style={{ color: colorTheme }}>
+                Author here
+            </div>
             <div className="buttons">
-                <div className="button">Twitter</div>
-                <div className="button">New Quote</div>
+                <div className="button" style={{ backgroundColor: colorTheme }}>
+                    Twitter
+                </div>
+                <div className="button" style={{ backgroundColor: colorTheme }}>
+                    New Quote
+                </div>
             </div>
         </div>
     );
